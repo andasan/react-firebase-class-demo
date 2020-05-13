86c9bf338ef7fad7e797e4022028a55c5b8ef789
Francois Polo <andasan@gmail.com>
2020-05-13 05:58:49 -0700

    added firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,36 @@
-import firebase from "firebase/app";
-// import database from "firebase/database";
+// import firebase from "firebase/app";
+// // import database from "firebase/database";
+// console.log("key: ",process.env.REACT_APP_AUTH_DOMAIN,)
+// const config = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APPID
+// };
+// firebase.initializeApp(config);
 
-const config = {
+// export default firebase;
+
+// let firebaseCache;
+
+// //getFirebase() function will ensure we only call initializeApp one time.
+// export const getFirebase = () => {
+//     if (firebaseCache) {
+//         return firebaseCache;
+//     }
+
+//     firebase.initializeApp(config);
+//     firebaseCache = firebase;
+//     return firebase;
+// };
+
+import firebase from 'firebase/app'
+import database from "firebase/database";
+
+const firebaseApp = firebase.initializeApp({
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -9,17 +38,7 @@ const config = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APPID
-};
+})
 
-let firebaseCache;
-
-//getFirebase() function will ensure we only call initializeApp one time.
-export const getFirebase = () => {
-    if (firebaseCache) {
-        return firebaseCache;
-    }
-
-    firebase.initializeApp(config);
-    firebaseCache = firebase;
-    return firebase;
-};
+// Export the database for components to use.
+export const db = firebaseApp.database()
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { db } from "../firebase";
 
 const Home = () => {
-  const blogPosts = [
-    {
-      title: "My Second Blog Post",
-      content:
-        "<p>Fruitcake biscuit jujubes chupa chups brownie chocolate. Pastry jelly beans macaroon soufflé pastry marshmallow cake chocolate bar. Brownie jelly-o lemon drops sweet roll. Donut tiramisu sugar plum chocolate. Croissant muffin biscuit. Cheesecake liquorice pudding oat cake biscuit powder. Chocolate bar wafer brownie danish gummies. Pie gingerbread tart lollipop biscuit cupcake fruitcake sweet macaroon. Dragée icing bear claw sugar plum cake caramels. Dragée muffin halvah topping oat cake powder brownie. Biscuit jelly brownie pastry marshmallow croissant. Sesame snaps jelly chupa chups bonbon apple pie wafer halvah caramels cupcake. Oat cake cotton candy jelly.</p><p>Danish ice cream toffee brownie biscuit. Croissant carrot cake icing candy canes. Pastry cotton candy cake gummies jelly beans. Gingerbread macaroon pie chocolate cake cake tootsie roll. Marzipan dragée chocolate sweet lemon drops candy tiramisu candy canes sweet. Chupa chups toffee bonbon. Pastry sweet roll jelly-o donut candy jelly beans lemon drops. Chocolate cake cupcake soufflé marzipan jelly-o sweet lemon drops pastry candy canes. Candy macaroon macaroon fruitcake toffee. Dessert lollipop soufflé. Caramels biscuit pudding tootsie roll jelly fruitcake. Tiramisu bonbon liquorice chocolate bar chupa chups.</p>",
-      coverImage: "https://source.unsplash.com/collection/190727/1600x900",
-      coverImageAlt: "Another random kitten from placekitten.com.",
-      slug: "my-second-blog-post",
-      dateFormatted: "2300-08-07",
-      datePrettify: "August 7th, 2300"
-    },
-    {
-      title: "My First Blog Post",
-      content:
-        "<p>Cupcake ipsum dolor sit amet. Topping cake jelly-o biscuit marshmallow. Lemon drops topping donut biscuit toffee caramels caramels danish. Gingerbread chocolate cake chocolate. Chocolate cake jelly cotton candy gummi bears candy. Chupa chups sesame snaps dessert carrot cake chocolate liquorice gingerbread. Sesame snaps chupa chups tootsie roll croissant ice cream biscuit. Cotton candy fruitcake candy canes. Cake oat cake chocolate cake tiramisu chupa chups. Toffee jelly gingerbread gummi bears cotton candy. Liquorice tiramisu marzipan jelly beans jelly cheesecake candy. Topping cupcake bear claw carrot cake cookie dessert marzipan caramels. Bear claw liquorice cotton candy toffee jelly donut tart tiramisu chocolate. Brownie tiramisu pie halvah.</p>",
-      coverImage: "https://source.unsplash.com/collection/190728/1600x900",
-      coverImageAlt: "A random kitten from placekitten.com.",
-      slug: "my-first-blog-post",
-      dateFormatted: "2300-08-01",
-      datePrettify: "August 1st, 2300"
-    }
-  ];
+  const [loading, setLoading] = useState(true);
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  if (loading && !blogPosts.length) {
+    db
+      .ref("/posts")
+      .orderByChild("dateFormatted")
+      .once("value")
+      .then(snapshot => {
+        let posts = [];
+        const snapshotVal = snapshot.val();
+        if (snapshotVal) {
+          const snapshotVal = snapshot.val();
+          for (let slug in snapshotVal) {
+            posts.push(snapshotVal[slug]);
+          }
+        }
+        const newestFirst = posts.reverse();
+        setBlogPosts(newestFirst);
+        setLoading(false);
+        setBlogPosts(posts);
+      })
+      .catch(err => console.log(err.message));
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <>
       <h1>Blog posts</h1>
-      <br/>
+      <br />
       {blogPosts && blogPosts.map(blogPost => (
         <section key={blogPost.slug} className="card">
-          <img src={blogPost.coverImage} alt={blogPost.coverImageAlt} />
+          <img src={blogPost.coverImage} alt={blogPost.coverImageAlt} crossOrigin="anonymous" />
           <div className="card-content">
             <h2>
               <Link to={`/${blogPost.slug}`}>{blogPost.title}</Link> &mdash;{" "}
